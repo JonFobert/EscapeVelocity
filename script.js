f2019c6733dcbf9722a9f636b6ddf273d4e8bc9b
Jon Fobert <jonrfobert@gmail.com>
2019-02-10 19:37:13 -0500

    initial layout

---
+++ script.js
@@ -0,0 +1,5 @@
+const splashQuestion = document.querySelector('.splash-question');
+
+//document.addEventListener('DOMContentLoaded', function() {
+   splashQuestion.classList.add("no-task-entered")
+//}, false);
