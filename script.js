946588d5ef302d32d693be54fa1da7b9c4af7ea5
Jon Fobert <jonrfobert@gmail.com>
2019-02-10 20:52:15 -0500

    add basic page functionality

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const splashQuestion = document.querySelector('.splash-question');
+const form = document.querySelector('form');
+const habitField = document.querySelector('#habit-field')
 
-//document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
    splashQuestion.classList.add("no-task-entered")
-//}, false);
+}, false);
+
+
+
+let userHabit = ''
+
+form.addEventListener("submit", e => {
+	e.preventDefault();
+	console.log(habitField.value)
+	userHabit = habitField.value;
+	console.log(userHabit)
+	splashQuestion.style.display = "none";
+	CreateHabitEl();
+})
+
+function CreateHabitEl() {
+	console.log(userHabit)
+	let node = document.querySelector('.habit-heading');
+	let textNode = document.createTextNode(`my new habit will be ${userHabit}`)
+	node.appendChild(textNode);
+}
