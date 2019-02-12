8c9cf5115c3a2f33baf348cc761315b7858f6912
Jon Fobert <jonrfobert@gmail.com>
2019-02-11 22:36:07 -0500

    draw rocket nozzle

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+const canvas = document.querySelector('.animation');
+const context = canvas.getContext('2d');
+
 const splashQuestion = document.querySelector('.splash-question');
 const form = document.querySelector('form');
 const habitField = document.querySelector('#habit-field')
+
 
 document.addEventListener('DOMContentLoaded', function() {
    splashQuestion.classList.add("no-task-entered")
@@ -27,28 +31,96 @@ function CreateHabitEl() {
 }
 
 
-//Loading rocket images
+//create rocket image elemets
 
 let rocketNozzle = document.createElement("img");
-rocketNozzle.src = "rocketParts/spaceRocketParts_005.png"
-let rocketNozzle = 0
+rocketNozzle.src = "rocketParts/picOne.png"
+let rocketNozzleX = 0
+
+console.log(rocketNozzle.src)
 
 let rocketFrameBottom = document.createElement("img");
 rocketFrameBottom.src = "rocketParts/spaceRocketParts_017.png"
-let rocketFrameBottom = 0
+let rocketFrameBottomX = 0
 
 let rocketFrameTop = document.createElement("img");
 rocketFrameTop.src = "rocketParts/spaceRocketParts_009.png"
-let rocketFrameTop = 0
+let rocketFrameTopX = 0
 
 let rocketWindow = document.createElement("img");
 rocketWindow.src = "rocketParts/spaceRocketParts_015.png"
-let rocketWindow = 0
+let rocketWindowX = 0
 
 let rocketNoseCone = document.createElement("img");
 rocketNoseCone.src = "rocketParts/spaceRocketParts_002.png"
-let rocketNoseCone = 0
+let rocketNoseConeX = 0
 
 let rocketSideBooster = document.createElement("img");
-rrocketSideBooster.src = "rocketParts/spaceRocketParts_020.png"
-let rocketSideBooster = 0
+rocketSideBooster.src = "rocketParts/spaceRocketParts_020.png"
+let rocketSideBoosterX = 0
+
+
+function drawNozzle() {
+	context.drawImage(rocketNozzle,
+					  //source rectangle
+					  0, 0, 68, 40,
+					  //destination rectange. -1 to compensate for blank left of sprite
+					  0,0,68,40);
+}
+
+
+function draw() {
+	drawNozzle()
+}
+
+
+
+//adapted from https://stackoverflow.com/questions/31299509/call-a-function-when-html5-canvas-is-ready
+//because the images load asychronously, wait for  all the images to load before calling the 
+//main animation frame for the first time
+
+var images = [
+            'rocketParts/spaceRocketParts_017.png',
+            'rocketParts/spaceRocketParts_009.png',
+            'rocketParts/spaceRocketParts_015.png',
+            'rocketParts/spaceRocketParts_002.png',
+            'rocketParts/spaceRocketParts_020.png'
+          ];
+var imagesLoading = images.length;
+
+// Image loader.
+var loadImage = function(i) {
+ var img = new Image();
+ img.onload = function() {
+   images[i] = img;
+   --imagesLoading;
+   // Call the complete callback when all images loaded.
+   if (imagesLoading === 0) {
+     workDone();
+   }
+ };
+ img.src = images[i];
+};
+
+// Call upon all images loaded.
+var workDone = function() {
+	requestAnimationFrame(main)
+}
+
+// Start to load all images
+var i;
+for(i = 0; i < imagesLoading; ++i) {
+	loadImage(i);
+}
+
+//resource for rAF and main loop: https://developer.mozilla.org/en-US/docs/Games/Anatomy
+//main loop for the game. Keeps track of time and calls draw function to compute and 
+//draw each frame once the player hits a button to start a game ("restart game" or "start game"). The
+//main animation loop starts
+let lastTime = 0;
+function main(time) {
+		requestAnimationFrame(main);
+		deltaTime = time - lastTime;
+		lastTime = time;
+		draw(deltaTime, time);
+}
