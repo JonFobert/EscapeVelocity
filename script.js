045be1dcb876bba22bef8ae74a31f5ff1c1ecc6a
Jon Fobert <jonrfobert@gmail.com>
2019-02-12 21:48:28 -0500

    create objects for image positions

--- script.js
+++ script.js
@@ -29,70 +29,40 @@ function CreateHabitEl() {
 }
 
 
-//create rocket image elemets
+//create rocket image elements
 
-let rocketNozzle = document.createElement("img");
-rocketNozzle.src = "rocketParts/picOne.png"
-let rocketNozzleX = 0
+let rocketNozzleImg = document.createElement("img");
+rocketNozzleImg.src = "rocketParts/nozzle.png"
 
-console.log(rocketNozzle.src)
+let rocketFrameBottomImg = document.createElement("img");
+rocketFrameBottomImg.src = "rocketParts/frameBottom.png"
 
-let rocketFrameBottom = document.createElement("img");
-rocketFrameBottom.src = "rocketParts/spaceRocketParts_017.png"
-let rocketFrameBottomX = 0
+let rocketFrameMiddleImg = document.createElement("img");
+rocketFrameBottomImg.src = "rocketParts/frameMiddle.png"
 
-let rocketFrameTop = document.createElement("img");
-rocketFrameTop.src = "rocketParts/spaceRocketParts_009.png"
-let rocketFrameTopX = 0
+let rocketFrameTopImg = document.createElement("img");
+rocketFrameTopImg.src = "rocketParts/frameTop.png"
 
-let rocketWindow = document.createElement("img");
-rocketWindow.src = "rocketParts/spaceRocketParts_015.png"
-let rocketWindowX = 0
+let rocketWindowImg = document.createElement("img");
+rocketWindowImg.src = "rocketParts/window.png"
 
-let rocketNoseCone = document.createElement("img");
-rocketNoseCone.src = "rocketParts/spaceRocketParts_002.png"
-let rocketNoseConeX = 0
+let rocketNoseConeImg = document.createElement("img");
+rocketNoseConeImg.src = "rocketParts/noseCone.png"
 
-let rocketSideBooster = document.createElement("img");
-rocketSideBooster.src = "rocketParts/spaceRocketParts_020.png"
-let rocketSideBoosterX = 0
+let rocketSideBoosterImg = document.createElement("img");
+rocketSideBoosterImg.src = "rocketParts/sideBooster.png"
 
-
-function drawNextImage() {
-	
-}
-
-function drawNozzle() {
-	context.drawImage(rocketNozzle,
-					  //source rectangle
-					  0, 0, 68, 40,
-					  //destination rectange. -1 to compensate for blank left of sprite
-					  0,0,68,40);
-}
-
-
-//This is dangerous, because if all the images are not yet loaded the button will not do anything.
-//Consider a loading screen or a way to disable input.
-completedButton.addEventListener('click', e => {
-	if (imagesLoaded) {
-		drawNozzle()
-	}
-})
-
-
-//adapted from https://stackoverflow.com/questions/31299509/call-a-function-when-html5-canvas-is-ready
-//because the images load asychronously, wait for  all the images to load before calling the 
-//main animation frame for the first time
+//make sure all the images are loaded before trying to draw then in the canvas
 
 let imagesLoaded = false
-
 var images = [
-			'rocketParts/picOne.png',
-            'rocketParts/spaceRocketParts_017.png',
-            'rocketParts/spaceRocketParts_009.png',
-            'rocketParts/spaceRocketParts_015.png',
-            'rocketParts/spaceRocketParts_002.png',
-            'rocketParts/spaceRocketParts_020.png'
+			'rocketParts/Nozzle.png',
+			'rocketParts/frameBottom.png',
+            'rocketParts/frameMiddle.png',
+            'rocketParts/frameTop.png',
+            'rocketParts/window.png',
+            'rocketParts/noseCone.png',
+            'rocketParts/sideBooster.png'
           ];
 var imagesLoading = images.length;
 
@@ -110,10 +80,143 @@ var loadImage = function(i) {
  img.src = images[i];
 };
 
-// Call upon all images loaded.
-var workDone = function() {
-	drawNozzle();
+// Start to load all images
+var i;
+for(i = 0; i < imagesLoading; ++i) {
+	loadImage(i);
 }
+
+//Define the location of the images for drawing.
+
+rocketNozzle = {
+	width: 68,
+	height: 40,
+	xPos: 450,
+	yPos: 450
+};
+
+frameBottom = {
+	width: 68,
+	height: 64,
+	xPos: 450,
+	yPos: 386
+};
+
+frameMiddleLower = {
+	width: 68,
+	height: 64,
+	xPos: 450,
+	yPos: 322
+};
+
+frameMiddleUpper = {
+	width: 68,
+	height: 64,
+	xPos: 450,
+	yPos: 258
+}
+
+frameTop = {
+	width: 68,
+	height: 64,
+	xPos: 450,
+	yPos: 192
+};
+
+rocketWindow = {
+	width: 28,
+	height: 28,
+	xPos: 470,
+	yPos: 208
+}
+
+noseCone = {
+	width: 68,
+	height: 78,
+	xPos: 450,
+	yPos: 114
+}
+
+leftSideBooster = { 422 365
+	width: 29,
+	height: 170,
+	xPos: 422,
+	yPos: 315
+}
+
+middleSideBooster = {468 346
+	width: 29,
+	height: 170,
+	xPos: 468,
+	yPos: 296
+}
+
+rightSideBooster = { 516 365
+	width: 29,
+	height: 170,
+	xPos: 516,
+	yPos: 315	
+}
+
+//When the completed button is pressed draw the next image.
+function drawNextImage() {
+	context.drawImage(rocketNozzleImg,
+					  //source rectangle
+					  0, 0, 68, 40,
+					  //destination rectange
+					  0,0,68,40);
+}
+
+function drawNozzle() {
+	context.drawImage(rocketNozzleImg,
+					  //source rectangle
+					  0, 0, 68, 64,
+					  //destination rectange
+					  0,0,68,40);
+}
+
+
+//This is dangerous, because if all the images are not yet loaded the button will not do anything.
+//Consider a loading screen or a way to disable input.
+completedButton.addEventListener('click', e => {
+	if (imagesLoaded) {
+		drawNextImage()
+	}
+})
+
+
+//adapted from https://stackoverflow.com/questions/31299509/call-a-function-when-html5-canvas-is-ready
+//because the images load asychronously, wait for all the images to load first
+//keeping this information up to date in two separate places is bad design.
+//See if you can find some other way to do this
+
+
+let imagesLoaded = false
+
+var images = [
+			'rocketParts/Nozzle.png',
+			'rocketParts/frameBottom.png',
+            'rocketParts/frameMiddle.png',
+            'rocketParts/frameTop.png',
+            'rocketParts/window.png',
+            'rocketParts/noseCone.png',
+            'rocketParts/sideBooster.png'
+          ];
+var imagesLoading = images.length;
+
+// Image loader.
+var loadImage = function(i) {
+ var img = new Image();
+ img.onload = function() {
+   images[i] = img;
+   --imagesLoading;
+   // Call the complete callback when all images loaded.
+   if (imagesLoading === 0) {
+     imagesLoaded = true;
+   }
+ };
+ img.src = images[i];
+};
 
 // Start to load all images
 var i;
@@ -121,3 +224,7 @@ for(i = 0; i < imagesLoading; ++i) {
 	loadImage(i);
 }
 
+
+myImages = [
+
+]
